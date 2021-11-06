a2d0976c657edd5519d76c9d2c0d7aec5ff12c92
garvit148 <61928763+garvit148@users.noreply.github.com>
2021-11-06 11:10:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -555,13 +555,13 @@ async def auto_filter(client, message):
                 for file in files
             ]
         else:
-            btn = [[
+            btn = [[[
                     InlineKeyboardButton(text=f"{file.file_name}", callback_data=f'files#{file.file_id}')
                 ],[
                 InlineKeyboardButton(text=f"☝ Size of this file ☝ = , {get_size(file.file_size)}",callback_data=f'files_#{file.file_id}')
                 ]
                 for file in files
-            ]
+            ]]
 
         if offset != "":
             key = f"{message.chat.id}-{message.message_id}"
